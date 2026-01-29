3583d6b83074d4b546a167abdefe9ffc7020a72a
tecnologiasemedbraganca-hub <tecnologiasemedbraganca@gmail.com>
2026-01-29 15:47:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot WhatsApp rodando 🚀"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print(data)  # depois vamos tratar a mensagem
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
