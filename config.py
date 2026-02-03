3d8e034c97f4ae69cc1bb80658fa8d4e4e2111da
tecnologiasemedbraganca-hub <tecnologiasemedbraganca@gmail.com>
2026-02-03 15:08:55 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID")
 PORT = int(os.environ.get("PORT", 10000))
+
