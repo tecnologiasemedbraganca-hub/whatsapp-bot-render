8fb72cdaf505ce8b7185e5fd3564152e25059438
tecnologiasemedbraganca-hub <tecnologiasemedbraganca@gmail.com>
2026-02-03 14:32:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def home():
     return "Bot WhatsApp Caeté rodando 🚀"
 
 if __name__ == "__main__":
+
     criar_tabelas()
-    app.run(host="0.0.0.0", port=PORT)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
