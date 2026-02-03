0a7161b0ffd02da3c5d28e41d0664be0ffffb4c7
tecnologiasemedbraganca-hub <tecnologiasemedbraganca@gmail.com>
2026-02-03 12:24:27 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID")
+PORT = int(os.environ.get("PORT", 10000))
