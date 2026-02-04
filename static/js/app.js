6be0471a57beb82702e9607f8a98b575d620b86b
tecnologiasemedbraganca-hub <tecnologiasemedbraganca@gmail.com>
2026-02-04 00:18:08 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,13 @@
+document.querySelectorAll(".conversation").forEach(item => {
+    item.addEventListener("click", () => {
+
+        document
+            .querySelectorAll(".conversation")
+            .forEach(c => c.classList.remove("active"));
+
+        item.classList.add("active");
+
+        console.log("Conversa selecionada");
+    });
+});
 
